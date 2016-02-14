0d9647f21dd467257cb814aa6d8b5f4938682319
Zach Gavin <gavinzach@gmail.com>
2016-02-14 12:10:18 -0500

    Write searchable module.

---
+++ lib/searchable.rb
@@ -0,0 +1,32 @@
+# This file will be a model to emulate 
+# ActiveRecord's searchable module
+
+module Searchable
+	# This where method takes a hash argument
+	# of values by column to match with records
+	# in our db
+	def where(params)
+		# We map the params to the correct heredoc syntax
+		where_line = params
+			.map { |key, value| "#{key} = ?" }
+    	.join(" AND ")
+
+    # we store the results to be parsed
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
+	end
+end
+
+
+# And we actually grant SQLObject class this capability
+class SQLObject
+	extend Searchable
+end
