40f2cc92b5d727f5770b61a8a39f126b5f34d818
Zach Gavin <gavinzach@gmail.com>
2016-03-08 11:33:38 -0500

    Remove different sql file and fix typo in db_connection.

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -3,8 +3,8 @@ require 'sqlite3'
 PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
 # https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
 ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
-CATS_SQL_FILE = File.join(ROOT_FOLDER, 'cats.sql')
-CATS_DB_FILE = File.join(ROOT_FOLDER, 'cats.db')
+PROJECTS_SQL_FILE = File.join(ROOT_FOLDER, 'projects.sql')
+PROJECTS_DB_FILE = File.join(ROOT_FOLDER, 'projects.db')
 
 class DBConnection
   def self.open(db_file_name)
@@ -17,12 +17,12 @@ class DBConnection
 
   def self.reset
     commands = [
-      "rm '#{CATS_DB_FILE}'",
-      "cat '#{CATS_SQL_FILE}' | sqlite3 '#{CATS_DB_FILE}'"
+      "rm '#{PROJECTS_DB_FILE}'",
+      "cat '#{PROJECTS_SQL_FILE}' | sqlite3 '#{PROJECTS_DB_FILE}'"
     ]
 
     commands.each { |command| `#{command}` }
-    DBConnection.open(CATS_DB_FILE)
+    DBConnection.open(PROJECTS_DB_FILE)
   end
 
   def self.instance
